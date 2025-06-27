50ef0121a02550a7889777d4dc9012bc99160aa4
Leo Victor <contato.leandrovmoreira@gmail.com>
2025-06-27 20:44:43 -0300

    Aplicando .gitignore e preparando push limpo

---
+++ SistemaBiblioteca/SistemaBiblioteca/Program.cs
@@ -0,0 +1,13 @@
+﻿using SistemaBiblioteca;
+
+
+
+Console.Write("Digite o nome do livro: ");
+string nome = Console.ReadLine();
+Console.Write("Digite o autor do livro: ");
+string autor = Console.ReadLine();
+Console.Write("Digite o número de paginas: ");
+int numeropaginas = int.Parse(Console.ReadLine());
+
+Livros livro = new Livros();
+livro.adicionar_livros(nome, autor, numeropaginas);
